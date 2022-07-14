ae2efbe8dd9337cbbc405a60393a06f9ccf74159
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-07-14 19:09:31 -0300

    commit reducer ready

--- src/Redux/reducer/Reducer.js
+++ src/Redux/reducer/Reducer.js
@@ -5,9 +5,48 @@ import {
     REMOVE_MOVIE_DETAIL,
 } from '../action/Actions';
 
-const initialState =  {
+const initialState = {
     moviesFavourities: [],
     moviesLoaded: [],
     moviesDeatail: {}
 }
 
+function rootReducer(state = initialState, action) {
+    if (action.type === ADD_MOVIE_FAVORITE) {
+        return {
+            ...state,
+            moviesFavourities: [...state.moviesFavourities.concat(action.payload)]
+        }
+    }
+    
+    if (action.type === GET_MOVIES) {
+        return {
+            ...state,
+            moviesLoaded: [...state.payload.Search]
+        }
+    }
+
+    if(action.type === GET_MOVIES_DETAIL ){
+        return{
+            ...state,
+            moviesDeatail: action.payload
+        }
+    }
+
+    if(action.type ===REMOVE_MOVIE_DETAIL){
+        return{
+            ...state,
+            moviesFavourities:  state.moviesFavourities.filter( movie =>{
+                movie.id !== action.payload
+            })
+        }
+    }
+}
+
+
+
+
+
+
+
+export default rootReducer;
