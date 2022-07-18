3db247e35881dbad426dcb925392593e5105abbb
Nicoovied0 <briannicolasoviedo@hotmail.com>
2022-07-18 19:11:15 -0300

    commit initial css landingPage

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,23 +1,30 @@
 import React from 'react'
 import img from '../img/logoapp.svg'
+import { Link } from "react-router-dom";
+import '../Css/LandingPage.css'
 
 const LandingPage = () => {
   return (
     <div>
-        <div>
-        <img src={img} alt='logo' className='Img-logo'/>
-        </div>
-        <div>
-            <h2>Who is watching?</h2>
-        </div>
-        <div>
-            
-            <h1>U</h1>
-            <h3>User</h3>
-        </div>
-        <div>
-            <h4>MANAGE PROFILES</h4>
-        </div>
+      <div>
+        <img src={img} alt='logo' className='Img-logo' />
+      </div>
+      <div>
+        <h2>Who is watching?</h2>
+      </div>
+      <div>
+        <Link to="/home">
+          <span className='Btn-home'>
+          <h1>U</h1>
+          <h3>User</h3>
+          </span>
+          
+        </Link>
+
+      </div>
+      <div>
+        <h4>MANAGE PROFILES</h4>
+      </div>
     </div>
   )
 }
